7922716c0d8a7b0cd1bd377b923884d29153f98a
WilliamJacobsen2 <150071621+WilliamJacobsen2@users.noreply.github.com>
2023-11-09 12:13:33 -0700

    Add files via upload

---
+++ rocketv3.py
@@ -0,0 +1,53 @@
+
+# Moffett, 11/9/2023
+# rocketship3.py
+# ASCII ART OF A ROCKET, testing loops
+
+size=3
+#size cannot = numbers that lead to text wrapping around the screen (for me its high 40's)
+
+def TopJet():
+    for i in range(1, size+1):
+        print('|'
+            + "." * (size-i)
+            + "/\\" * (i)
+            + ".." * ((size-i))
+            + "/\\" * (i)
+            + "." * (size-i)
+            + "|")
+def BotJet():
+    for i in range(1, size+1):
+        print('|'
+            + "." * (i-1)
+            + "\\/" * ((size-i)+1)
+            + ".." * (i-1)
+            + "\\/" * ((size-i)+1)
+            + "." * (i-1)
+            + "|")
+def Bar():
+        print(("+")+("=*"* (size*2))+("+"))
+#jetfuel issue fixed 
+def Cone():
+    for i in range(1, (size*2)+1):
+        print(
+            " " * ((size*2)-i)
+            + "/" * (i)
+            + "**"
+            + "\\" * (i))
+
+def DBody():
+    TopJet()
+    BotJet()
+def HBody():
+    BotJet()
+    TopJet()
+    
+
+#code
+Cone()
+Bar()
+DBody()
+Bar()
+HBody()
+Bar()
+Cone()
